87a0dc5f124fad9e42544c2a066b781ae94fee06
carlos <carlos_lovera@outlook.es>
2023-11-23 16:39:15 -0300

    parte1

--- java.js
+++ java.js
@@ -1,1 +1,14 @@
-console.log
+document.addEventListener('DOMContentLoaded', function() {
+  var toggleButton = document.getElementById('toggleButton');
+  var listaDesplegable = document.getElementById('listaDesplegable');
+
+  toggleButton.addEventListener('click', function() {
+      if (listaDesplegable.style.display === 'none') {
+          listaDesplegable.style.display = 'block';
+          toggleButton.textContent = 'Categorias -';
+      } else {
+          listaDesplegable.style.display = 'none';
+          toggleButton.textContent = 'Categorias +';
+      }
+  });
+});
